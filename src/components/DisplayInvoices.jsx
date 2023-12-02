49281538b00c46195bf71535abafc9df0e7f45ed
unknown <s4shreyadhingra@gmail.com>
2023-12-02 23:00:25 +0530

    added table view for invoices

--- src/components/DisplayInvoices.jsx
+++ src/components/DisplayInvoices.jsx
@@ -3,6 +3,7 @@ import { deleteInvoice, editInvoice } from "../reducers/InvoiceSlice";
 
 import CardView from "./CardView";
 import TableView from "./TableView";
+import styles from './DisplayInvoices.module.css';
 
 const DisplayInvoices = ({ type }) => {
   const dispatch = useDispatch();
@@ -16,20 +17,43 @@ const DisplayInvoices = ({ type }) => {
   };
 
   return (
-    <div>
-      DisplayInvoices
-      {invoiceList.map((invoice, index) =>
-        type === "card" ? (
+    <div className="">
+      Invoices
+      {type === "table" && (
+        <table className={styles.table}>
+          <thead className={styles["table-heading"]}>
+            <tr>
+              <th>Invoice No.</th>
+              <th>Date</th>
+              <th>Bill from</th>
+              <th>Bill to</th>
+              <th>Total Items</th>
+              <th>Amount</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          
+    <tbody className={styles["table-body"]}>
+          {invoiceList.map((invoice, index) => (
+            <TableView
+              key={index}
+              invoice={invoice}
+              deleteInvoice={removeInvoice}
+              editInvoice={editInvoice}
+            />
+          ))}
+          </tbody>
+        </table>
+      )}
+      {type === "card" &&
+        invoiceList.map((invoice, index) => (
           <CardView
             key={index}
             invoice={invoice}
             deleteInvoice={removeInvoice}
             editInvoice={editInvoice}
           />
-        ) : (
-          <TableView key={index} invoice={invoice} />
-        )
-      )}
+        ))}
     </div>
   );
 };
--- src/components/TableView.jsx
+++ src/components/TableView.jsx
@@ -1,6 +1,59 @@
-const TableView = () => {
+import { useState } from "react";
+
+import { MdDelete } from "react-icons/md";
+import { FaEdit } from "react-icons/fa";
+import { FaEye } from "react-icons/fa";
+import styles from "./TableView.module.css";
+import InvoiceModal from "./InvoiceModal";
+import { Link } from "react-router-dom";
+
+const TableView = (props) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const invoice = props.invoice;
+
+  const viewInvoiceHandler = () => setIsModalOpen(true);
+  const deleteInvoiceHandler = () => {
+    props.deleteInvoice(invoice.invoiceNumber);
+  };
+
   return (
-    <div>TableView</div>
-  )
-}
-export default TableView
+    <>
+      <tr>
+        <td>#{invoice.invoiceNumber}</td>
+        <td>{invoice.dateOfIssue}</td>
+        <td>{invoice.billFrom}</td>
+        <td>{invoice.billTo}</td>
+        <td>{invoice.items.length}</td>
+        <td>{invoice.total}</td>
+        <td className={styles.actions}>
+            <button onClick={viewInvoiceHandler}>
+              <FaEye />
+            </button>
+            <Link to={`/edit-invoice/${invoice.invoiceNumber}`}>sample</Link>
+            <button>
+              <FaEdit />
+            </button>
+            <button onClick={deleteInvoiceHandler}>
+              <MdDelete /> 
+            </button>
+        </td>
+      </tr>
+
+      {isModalOpen && (
+        <InvoiceModal
+          showModal={isModalOpen}
+          closeModal={() => setIsModalOpen(false)}
+          info={invoice}
+          items={invoice.items}
+          currency={invoice.currency}
+          subTotal={invoice.subTotal}
+          taxAmmount={invoice.taxAmmount}
+          discountAmmount={invoice.discountAmmount}
+          total={invoice.total}
+        />
+      )}
+    </>
+  );
+};
+export default TableView;
