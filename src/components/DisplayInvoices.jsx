d86c0691d08601d73c950ca343869c28f5602623
unknown <s4shreyadhingra@gmail.com>
2023-12-05 00:38:23 +0530

    added sort invoices functionality

--- src/components/DisplayInvoices.jsx
+++ src/components/DisplayInvoices.jsx
@@ -1,19 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { editInvoice } from "../reducers/InvoiceSlice";
 import { getInvoices, deleteInvoice } from "../actions/invoices";
 
+import { FaChevronCircleDown } from "react-icons/fa";
+import { FaArrowDown } from "react-icons/fa";
+import { FaArrowUp } from "react-icons/fa";
 import CardView from "./CardView";
 import TableView from "./TableView";
 import styles from "./DisplayInvoices.module.css";
 
 const DisplayInvoices = ({ type }) => {
+  const [ sortType, setSortType ] = useState("default");
   const dispatch = useDispatch();
   const invoiceListDefault = useSelector((state) => state.invoices.invoices);
-  let invoiceList = [...invoiceListDefault];
   
-        
-  invoiceList.sort((a, b) => a.data.invoiceNumber - b.data.invoiceNumber);
+  let invoiceList = [];
+  if(sortType === "default") {
+    invoiceList = [...invoiceListDefault];
+  }
+  else if(sortType === "increasing") {
+    console.log(`in increasin`)
+    invoiceList.sort((a, b) => a.data.invoiceNumber - b.data.invoiceNumber)
+    console.log(`in display ${JSON.stringify(invoiceList)}`)
+  }
+  else if(sortType === "decreasing") {
+    invoiceList.sort((a, b) => b.data.invoiceNumber - a.data.invoiceNumber)
+  }
 
   useEffect(() => {
     dispatch(getInvoices());
@@ -27,8 +40,27 @@ const DisplayInvoices = ({ type }) => {
     dispatch(editInvoice(invoice));
   };
 
+
   return (
-    <div className="">
+    <div className={styles.parent}>
+      <div className={styles.dropdown}>
+        <button className={styles.dropbtn}>
+          Sort Invoices
+          <FaChevronCircleDown />
+        </button>
+        <div className={styles["dropdown-content"]}>
+          <ul>
+            <li style={{ borderBottom: "1px solid cadetblue" }} onClick={() => setSortType("increasing")}>
+              Increasing
+              <FaArrowUp style={{ left: "10px" }} />
+            </li>
+            <li onClick={() => setSortType("decreasing")}>
+              Decreasing
+              <FaArrowDown />
+            </li>
+          </ul>
+        </div>
+      </div>
       <h3 className={styles.heading}>Invoices List</h3>
       {type === "table" && (
         <table className={styles.table}>
